9871f9bea02aedce67c81da7fd0ca6c0fd433929
dieegot3 <diego_diaz96@outlook.com>
2023-06-12 10:35:56 -0300

    deploy a vercel

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./NavBar.css";
+import "./Navbar.css";
 import { CartWidget } from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
 import { Categories } from "./Categories/Categories";
