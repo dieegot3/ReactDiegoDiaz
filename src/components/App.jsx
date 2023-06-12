b6125d4bb07252ac4ed1adcbc3effa7d8b4e8908
dieegot3 <diego_diaz96@outlook.com>
2023-06-12 10:58:23 -0300

    subida a vercel cambio api key

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from "./Navbar/Navbar";
 import { ItemListContainer } from "./ItemListContainer/ItemListContainer";
 import { ItemDetailContainer } from "./ItemDetailContainer/ItemDetailContainer";
 import { Checkout } from "./Checkout/Checkout";
@@ -14,7 +13,6 @@ export const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <ToastContainer />
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
